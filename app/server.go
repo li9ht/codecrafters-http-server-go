26a58ce32bf5b22bd2ef8737c5ffd89c4dc9a4ee
fird <fird01@gmail.com>
2024-07-12 17:54:36 +0800

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	// Uncomment this block to pass the first stage
 )
 
@@ -23,6 +24,13 @@ func main() {
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	req := make([]byte, 1024)
+	conn.Read(req)
+	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		conn.Close()
+		return
 	}
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
